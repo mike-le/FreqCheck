0a7b66a61a86273e133a9e915e60bf8d04146820
mike-le <michael.le@richmond.edu>
2019-05-15 21:24:48 -0400

    Initialize project with Mocha tests

---
+++ stemmer.js
@@ -0,0 +1,16 @@
+const stem = function stemmer (w){
+    
+    c = "[^aeiou]",          // consonant
+    v = "[aeiouy]",          // vowel
+    C = c + "[^aeiouy]*",    // consonant sequence
+    V = v + "[aeiou]*",      // vowel sequence
+
+    mgr0 = "^(" + C + ")?" + V + C,               // [C]VC... is m>0
+    meq1 = "^(" + C + ")?" + V + C + "(" + V + ")?$",  // [C]VC[V] is m=1
+    mgr1 = "^(" + C + ")?" + V + C + V + C,       // [C]VCVC... is m>1
+    vowel_in_stem = "^(" + C + ")?" + v;          // vowel in stem
+
+    return w;
+}
+
+module.exports = { stem }
---
+++ test.js
@@ -0,0 +1,82 @@
+const assert = require('assert')
+const { stem } = require('./stemmer.js')
+
+it('basic -ing', () => {
+  assert.equal(stem("testing"), "test")
+})
+
+it('basic -ed', () => {
+  assert.equal(stem("tested"), "test")
+})
+
+it('basic -s', () => {
+  assert.equal(stem("tests"), "test")
+})
+
+it('basic -es', () => {
+  assert.equal(stem("passes"), "pass")
+})
+
+it('basic -eed', () => {
+  assert.equal(stem("agreed"), "agree")
+})
+
+it('-ies with stem ending in -y', () => {
+  assert.equal(stem("ponies"), "pony")
+})
+
+it('-ing with 1 syllable', () => {
+  assert.equal(stem("sing"), "sing")
+})
+
+it('-ing with stem ending in e', () => {
+  assert.equal(stem("filing"), "file")
+})
+
+it('-ed with stem ending in e', () => {
+  assert.equal(stem("conflated"), "conflate")
+})
+
+it('-ed with stem ending in e', () => {
+  assert.equal(stem("troubled"), "trouble")
+})
+
+it('-ed with stem ending in e', () => {
+  assert.equal(stem("sized"), "size")
+})
+
+it('-ing with double consonants', () => {
+  assert.equal(stem("hopping"), "hop")
+})
+
+it('-ing with double consonants', () => {
+  assert.equal(stem("controlling"), "control")
+})
+
+it('-ed with double consonants', () => {
+  assert.equal(stem("tanned"), "tan")
+})
+
+it('-ing with stem ending in double consonants', () => {
+  assert.equal(stem("falling"), "fall")
+})
+
+it('-ing with stem ending in double consonants', () => {
+  assert.equal(stem("hissing"), "hiss")
+})
+
+it('-ed with stem ending in double consonants', () => {
+  assert.equal(stem("fizzed"), "fizz")
+})
+
+it('Irregular -es: leaves', () => {
+  assert.equal(stem("leaves"), "leaf")
+})
+
+it('Irregular -es: ties', () => {
+  assert.equal(stem("ties"), "tie")
+})
+
+
+
+
